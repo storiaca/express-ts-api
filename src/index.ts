b8fdb5b08f62594bea426e6b4451ad8b5a1b224d
storiaca <storiaca@gmail.com>
2019-12-22 12:44:10 +0100

    Import packages to index.ts file

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Required External Modules
+ */
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+/**
+ * App Variables
+ */
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Server Activation
+ */
+
+/**
+ * Webpack HMR Activation
+ */
