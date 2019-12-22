b1e4495a048cd602d764d4e252d69de0d9b70f20
storiaca <storiaca@gmail.com>
2019-12-22 12:46:48 +0100

    Create app variables

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,13 @@ dotenv.config();
 /**
  * App Variables
  */
+if (!process.env.PORT) {
+  process.exit(1);
+}
 
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
 /**
  *  App Configuration
  */
